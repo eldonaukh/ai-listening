a2029961bc2c35caf518e288046fb31b74380a54
eldonaukh <eldonakh@gmail.com>
2025-12-06 22:25:14 +0800

    Add chatprocessor.py and move its logic from main.py

---
+++ utils/chatprocessor.py
@@ -0,0 +1,100 @@
+import pandas as pd
+from utils.ai import SentimentAnalyzer
+from utils.validator import KeywordSchema, ChatSchema, KeywordRow
+from typing import cast
+from pandera.typing import DataFrame
+
+
+class ChatProcessor:
+
+    def __init__(
+        self, keyword_df: DataFrame[KeywordSchema], analyzer: SentimentAnalyzer
+    ):
+        self._keyword_df = keyword_df
+        self.analyzer = analyzer
+
+    @property
+    def keyword_df(self):
+        return self._keyword_df
+
+    @property
+    def unique_headers(self) -> list[str]:
+        return list(self._keyword_df["headers"].unique())
+
+    @property
+    def generic_headers(self) -> list[str]:
+        return [header for header in self.unique_headers if "generic" in header]
+
+    @property
+    def non_generic_headers(self) -> list[str]:
+        return [header for header in self.unique_headers if "generic" not in header]
+
+    def process_chat_df(self, chat_df: DataFrame[ChatSchema]) -> DataFrame[ChatSchema]:
+        df = self._add_header_columns_to_chat_df(chat_df)
+        df = self._tag_keywords(df)
+
+        return df
+
+    def _get_keyword_rows_of_header(self, header: str) -> DataFrame[KeywordSchema]:
+        return self._keyword_df[self._keyword_df["headers"] == header]
+
+    def _tag_keywords(self, chat_df: DataFrame[ChatSchema]) -> DataFrame[ChatSchema]:
+        for ng in self.non_generic_headers:
+            self._apply_mask(chat_df, ng, skip_mask=None)
+
+        for g in self.generic_headers:
+            main = g.replace("_generic", "")
+            subbrand = [brand for brand in self.non_generic_headers if main in brand]
+            skip_mask = chat_df[subbrand].any(axis=1)
+
+            self._apply_mask(chat_df, g, skip_mask=skip_mask)
+        return chat_df
+
+    def _apply_mask(
+        self,
+        chat_df: DataFrame[ChatSchema],
+        header: str,
+        skip_mask: pd.Series | None,
+        message_column: str = "messageBody",
+    ) -> DataFrame[ChatSchema]:
+        matched = self._get_keyword_rows_of_header(header)
+
+        target = (
+            chat_df[message_column]
+            if skip_mask is None
+            else chat_df.loc[~skip_mask, message_column]
+        )
+        for row in cast(list[KeywordRow], matched.itertuples(index=False)):
+            mask_keyword = target.str.contains(row.keyword, case=False, na=False)
+            if row.required_kw:
+                mask_required = target.str.contains(
+                    row.required_kw, case=False, na=False
+                )
+                final_mask = mask_required & mask_keyword
+            else:
+                final_mask = mask_keyword
+
+        if skip_mask is not None:
+            chat_df[~skip_mask, header] = chat_df.loc[
+                ~skip_mask, header
+            ] | final_mask.astype(int)
+        else:
+            chat_df[header] = chat_df[header] | final_mask.astype(int)
+
+        return chat_df
+
+    def _add_header_columns_to_chat_df(
+        self, chat_df: DataFrame[ChatSchema]
+    ) -> DataFrame[ChatSchema]:
+        for header in self.unique_headers:
+            chat_df[header] = 0
+        return chat_df
+    
+    def pass_to_llm(self, row: pd.Series, header: str, keywords: str) -> pd.Series:
+        data = (
+            f"Formula Brand: {header}, Keyword: {keywords}, Message: {row["messageBody"]}"
+        )
+        response = self.analyzer.analyze(data)
+        row[header] = response["sentiment"]
+        row["Reason"] = response["reason"]
+        return row
