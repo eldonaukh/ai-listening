8cfcb9d600d33d95f197a18e37c3c125532bc6b2
eldonaukh <eldonakh@gmail.com>
2025-12-07 00:08:28 +0800

    Remove unused import in main.py

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 import pandas as pd
-import json
-from pathlib import Path
-from utils.ai import get_analyzer, SentimentAnalyzer
-from utils.loader import KeywordSchema, ChatSchema, DataLoader
-from typing import Any, NamedTuple, cast, Optional
-from tqdm import tqdm
-import pandera.pandas as pa
-from pandera.typing import DataFrame, Series
-
-
-class KeywordRow(NamedTuple):
-    brand: str
-    product: str
-    keyword: str
-    required_product: str | None
-    required_kw: str | None
+from utils.ai import get_analyzer
+from utils.validator import KeywordSchema, ChatSchema, KeywordRow
+from typing import Any, cast
+from pandera.typing import DataFrame
 
 
 def pass_to_llm(row: pd.Series, header: str, keywords: str) -> pd.Series:
@@ -92,7 +80,7 @@ def process_chat(
             if row_req_kw:
                 keywords.extend(row_req_kw.split("|"))
             row_kw: Any = kw_row.keyword
-            keywords.extend(row_kw)
+            keywords.append(row_kw)
         kw_str = ", ".join(set(keywords))
 
         # ask llm for sentiment check
@@ -103,114 +91,22 @@ def process_chat(
     return df
 
 
-class ChatProcessor:
-
-    def __init__(
-        self, keyword_df: DataFrame[KeywordSchema], analyzer: SentimentAnalyzer
-    ):
-        self._keyword_df = keyword_df
-        self._analyzer = analyzer
-
-    @property
-    def keyword_df(self):
-        return self._keyword_df
-
-    @property
-    def unique_headers(self) -> list[str]:
-        return list(self._keyword_df["headers"].unique())
-
-    @property
-    def generic_headers(self) -> list[str]:
-        return [header for header in self.unique_headers if "generic" in header]
-
-    @property
-    def non_generic_headers(self) -> list[str]:
-        return [header for header in self.unique_headers if "generic" not in header]
-
-    def process_chat_df(self, chat_df: DataFrame[ChatSchema]) -> DataFrame[ChatSchema]:
-        df = self._add_header_columns_to_chat_df(chat_df)
-        df = self._tag_keywords(df)
-
-        return df
-
-    def _get_keyword_rows_of_header(self, header: str) -> DataFrame[KeywordSchema]:
-        return self._keyword_df[self._keyword_df["headers"] == header]
-
-    def _tag_keywords(self, chat_df: DataFrame[ChatSchema]) -> DataFrame[ChatSchema]:
-        for ng in self.non_generic_headers:
-            self._apply_mask(chat_df, ng, skip_mask=None)
-
-        for g in self.generic_headers:
-            main = g.replace("_generic", "")
-            subbrand = [brand for brand in self.non_generic_headers if main in brand]
-            skip_mask = chat_df[subbrand].any(axis=1)
-
-            self._apply_mask(chat_df, g, skip_mask=skip_mask)
-        return chat_df
-
-    def _apply_mask(
-        self,
-        chat_df: DataFrame[ChatSchema],
-        header: str,
-        skip_mask: pd.Series | None,
-        message_column: str = "messageBody",
-    ) -> DataFrame[ChatSchema]:
-        matched = self._get_keyword_rows_of_header(header)
-
-        target = (
-            chat_df[message_column]
-            if skip_mask is None
-            else chat_df.loc[~skip_mask, message_column]
-        )
-        for row in cast(list[KeywordRow], matched.itertuples(index=False)):
-            mask_keyword = target.str.contains(row.keyword, case=False, na=False)
-            if row.required_kw:
-                mask_required = target.str.contains(
-                    row.required_kw, case=False, na=False
-                )
-                final_mask = mask_required & mask_keyword
-            else:
-                final_mask = mask_keyword
-
-        if skip_mask is not None:
-            chat_df[~skip_mask, header] = chat_df.loc[
-                ~skip_mask, header
-            ] | final_mask.astype(int)
-        else:
-            chat_df[header] = chat_df[header] | final_mask.astype(int)
-
-        return chat_df
-
-    def _add_header_columns_to_chat_df(
-        self, chat_df: DataFrame[ChatSchema]
-    ) -> DataFrame[ChatSchema]:
-        for header in self.unique_headers:
-            chat_df[header] = 0
-        return chat_df
-
-
 def main() -> None:
+    pass
     # get keywords from xlsx
-    loader = DataLoader("./files")
-    keyword_df = loader.get_keyword_df("keywords.xlsx")
+    # loader = DataLoader("./files")
+    # keyword_df = loader.get_keyword_df("keywords.xlsx")
 
-    # load all csv files in folders into one df
-    sheets = loader.get_chat_dfs("./chats")
-    # chat = base_path / "chats"
-    # subfolders: list[Path] = [f for f in chat.iterdir() if f.is_dir()]
-    # for sub in subfolders:
-    #     print("Start processing:", sub.name)
-    #     loader = DataLoader(base_path, "keywords.xlsx")
-    #     loader.get_chat_df_folder(sub)
-    #     df: DataFrame[ChatSchema] | None = loader.combine_chat()
-    if sheets is not None:
-        for sheet, df in sheets.items():
-            processed = process_chat(df=df, keyword_df=keyword_df)
-            sheets[sheet] = processed
-            print("Processed:", sheet)
+    # # load all csv files in folders into one df
+    # sheets = loader.get_chat_dfs("./chats")
+    # if sheets is not None:
+    #     for sheet, df in sheets.items():
+    #         processed = process_chat(df=df, keyword_df=keyword_df)
+    #         sheets[sheet] = processed
+    #         print("Processed:", sheet)
 
-    # export df into xlsx
-    loader.output_to_xlsx(sheets, filename="output.xlsx")
+    # # export df into xlsx
+    # loader.output_to_xlsx(sheets, filename="output.xlsx")
 
 
 if __name__ == "__main__":
